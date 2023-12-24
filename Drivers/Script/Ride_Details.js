3f604d018a34e179860dd704c54de8baff7bb71e
hosny <hosnymohammed2007@gmail.com>
2023-12-24 23:29:05 +0200

    Final Editing

---
+++ Drivers/Script/Ride_Details.js
@@ -0,0 +1,9 @@
+function Confirm_btn(){
+    alert("You just Confirmed the request")
+}
+function Refused_btn(){
+    alert("You just refused the request")
+}
+function Back_btn(){
+    window.location.assign("Rides_Request.html")
+}
---
+++ Drivers/Script/Rides_Request.js
@@ -0,0 +1,6 @@
+function Details(){
+    window.location.assign("Ride_Details.html")
+}
+function Confirm_btn(){
+    alert("You just Confirmed the request")
+}
